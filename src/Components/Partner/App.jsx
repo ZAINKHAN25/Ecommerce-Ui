7ea7081f1c0bf3ed37396c7c4a39ba238a7f5325
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-01-06 13:32:04 +0500

    Image lagai

--- src/Components/Partner/App.jsx
+++ src/Components/Partner/App.jsx
@@ -5,7 +5,7 @@ function Partner() {
     return (
         <div className='Partner'>
             <p>At Dejee’s Store, we are committed to sustainability and ethical business practices. We believe in preserving the environment and ensuring that our products are made in an ethical and sustainable manner.</p>
-            <img src="" alt="" />
+            <img src="https://www.onboardmeetings.com/wp-content/uploads/2022/07/Partnership-Agreement-2.jpeg" alt="" />
         </div>
     )
 }
