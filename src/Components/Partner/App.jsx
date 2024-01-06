3d597d87f593c2b463c3773d5cdcdf2c2facb866
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-01-06 22:40:08 +0500

    Partner wala kam hogya

--- src/Components/Partner/App.jsx
+++ src/Components/Partner/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
+
 
 function Partner() {
     return (
-        <div className='Partner'>
-            <p>At Dejee’s Store, we are committed to sustainability and ethical business practices. We believe in preserving the environment and ensuring that our products are made in an ethical and sustainable manner.</p>
-            <img src="https://www.onboardmeetings.com/wp-content/uploads/2022/07/Partnership-Agreement-2.jpeg" alt="" />
+        <div className='Partner mx-3 px-2 py-5'>
+            <p className='paraofpartner'>At Dejee’s Store, we are committed to sustainability and ethical business practices. We believe in preserving the environment and ensuring that our products are made in an ethical and sustainable manner.</p>
+            <img src="https://www.onboardmeetings.com/wp-content/uploads/2022/07/Partnership-Agreement-2.jpeg" className='imgofpartner' alt="" />
         </div>
     )
 }
