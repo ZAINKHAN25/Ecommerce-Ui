5b6441cd1fab5d9942158fc50bbaa3703d0bb95a
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-01-07 19:05:56 +0500

    Customer service

--- src/Pages/Home/App.jsx
+++ src/Pages/Home/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import "./App.css";
 
-import Navbar from '../../Components/Navbar/App.jsx'
-import IntroductionSection from '../../Components/IntroductionSection/App.jsx'
-import Asseenin from '../../Components/Asseenin/App.jsx'
-import Aboutus from '../../Components/Aboutus/App.jsx'
-import Whychoseus from '../../Components/Whychoseus/App.jsx'
-import Sales from '../../Components/Sales/App.jsx'
-import Categories from '../../Components/Categories/App.jsx'
-import NewArrivals from '../../Components/NewArrivals/App.jsx'
-import Partner from '../../Components/Partner/App.jsx'
+import Navbar from '../../Components/Navbar/App.jsx';
+import IntroductionSection from '../../Components/IntroductionSection/App.jsx';
+import Asseenin from '../../Components/Asseenin/App.jsx';
+import Aboutus from '../../Components/Aboutus/App.jsx';
+import Whychoseus from '../../Components/Whychoseus/App.jsx';
+import Sales from '../../Components/Sales/App.jsx';
+import Categories from '../../Components/Categories/App.jsx';
+import NewArrivals from '../../Components/NewArrivals/App.jsx';
+import Partner from '../../Components/Partner/App.jsx';
+import CustomerReviews from '../../Components/CustomerReviews/App.jsx';
 
 
 function Home() {
@@ -24,7 +25,7 @@ function Home() {
       <Categories />
       <NewArrivals />
       <Partner />
-{/*       Aj customer service wala kam bhi karna hai */}
+      <CustomerReviews/>
     </div>
   )
 }
