bf0384553ba1da1b6bf44ac265119a296612ad42
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-01-07 17:03:45 +0500

    Kam chal rhah ia

--- src/Components/Partner/App.jsx
+++ src/Components/Partner/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 function Partner() {
     return (
-        <div className='Partner mx-3 px-2 py-5'>
+        <div className='Partner px-2 py-5'>
             <p className='paraofpartner'>At Dejee’s Store, we are committed to sustainability and ethical business practices. We believe in preserving the environment and ensuring that our products are made in an ethical and sustainable manner.</p>
             <img src="https://www.onboardmeetings.com/wp-content/uploads/2022/07/Partnership-Agreement-2.jpeg" className='imgofpartner' alt="" />
         </div>
