258e1f1c5d07434c9eedcf13c482bca948075a92
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-01-12 09:49:49 +0500

    Work is going

--- src/Components/PreservingTradition/App.jsx
+++ src/Components/PreservingTradition/App.jsx
@@ -3,15 +3,13 @@ import "./App.css"
 
 function PreservingTradition() {
   return (
-    <div className='PreservingTradition'>
-        <h1></h1>
-        <p></p>
-        <div className="btnareaofpreservinttradition">
-            <button></button>
-            <button className='button'>
-              
-            </button>
-        </div>
+    <div className='p-5 PreservingTradition'>
+      <h1>Preserving Pakistani Tradition</h1>
+      <p></p>At Dejee’s Store, we are dedicated to preserving the rich tradition of Pakistani clothing. We believe that everyone should have access to traditional Pakistani clothing, which is why we offer a wide variety of products at affordable prices. Join us in celebrating the beauty of Pakistani clothing.
+      <div className="btnareaofpreservinttradition mt-3">
+        <button className='button'>Kameez</button>
+        <button className='button'>Clothes</button>
+      </div>
     </div>
   )
 }
