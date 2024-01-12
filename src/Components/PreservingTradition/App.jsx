ed3054e997b353c252518769e2020ac8d97d378d
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-01-12 10:26:05 +0500

    Preserving wala kam hogya

--- src/Components/PreservingTradition/App.jsx
+++ src/Components/PreservingTradition/App.jsx
@@ -6,9 +6,9 @@ function PreservingTradition() {
     <div className='p-5 PreservingTradition'>
       <h1>Preserving Pakistani Tradition</h1>
       <p></p>At Dejee’s Store, we are dedicated to preserving the rich tradition of Pakistani clothing. We believe that everyone should have access to traditional Pakistani clothing, which is why we offer a wide variety of products at affordable prices. Join us in celebrating the beauty of Pakistani clothing.
-      <div className="btnareaofpreservinttradition mt-3">
-        <button className='button'>Kameez</button>
-        <button className='button'>Clothes</button>
+      <div className="btnareaofpreservinttradition mt-5">
+        <button className='px-5 mx-2 py-2'>Kameez</button>
+        <button className='px-5 mx-2 py-2'>Clothes</button>
       </div>
     </div>
   )
